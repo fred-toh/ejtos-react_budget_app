8efa52100aba419ffe666f0af787a6b9fa953164
fred-toh <fredtoh@hotmail.com>
2024-05-17 02:03:36 -0400

    Debugged function to update Budget.

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,17 +1,19 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    const [newBudget, setNewBudget] = useState(budget);
-    const handleBudgetChange = (event) => {
-        setNewBudget(event.target.value);
+    const { budget, dispatch } = useContext(AppContext);
+    const changeBudget = (event) => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: event.target.value,
+        })
     }
     return (
 <div className='alert alert-secondary'>
 <span>Budget: £{budget}</span>
 
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<input type="number" step="10" value={budget} onChange={changeBudget}></input>
 </div>
     );
 };
